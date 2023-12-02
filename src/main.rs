1796d3271bee6662bec8dd1522e999440da85bc7
Jakub Cebulski <jakubceb@protonmail.com>
2023-12-02 11:02:19 +0100

    day2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,95 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::collections::HashMap;
+use atoi::atoi;
+use strum::IntoEnumIterator; // 0.17.1
+use strum_macros::EnumIter; // 0.17.1
+
+#[derive(Debug, PartialEq, Eq, Hash, EnumIter)]
+enum Color{
+    Red,
+    Green,
+    Blue,
+}
+
+fn game_valid(line: &str, limit: &HashMap<Color, u16>)->u16{
+    let mut game: HashMap<Color, u16> = HashMap::from([(Color::Red, 0), (Color::Green, 0), (Color::Blue, 0)]);
+    let reds: Vec<_> = line.match_indices("red").collect();
+    let greens: Vec<_> = line.match_indices("green").collect();
+    let blues: Vec<_> = line.match_indices("blue").collect();
+
+    let game_id = atoi::<u16>(line[5..].as_bytes()).unwrap();
+
+    for red_idx in reds{
+        let color_count = line[red_idx.0-3..red_idx.0].trim().parse::<u16>().unwrap();
+        let old_count = game.get(&Color::Red).unwrap();
+        if *old_count < color_count{
+           game.insert(Color::Red, color_count);
+        }
+    }
+
+    for blue_idx in blues{
+        let color_count = line[blue_idx.0-3..blue_idx.0].trim().parse::<u16>().unwrap();
+        let old_count = game.get(&Color::Blue).unwrap();
+        if *old_count < color_count{
+           game.insert(Color::Blue, color_count);
+        }
+    }
+
+        for green_idx in greens{
+        let color_count = line[green_idx.0-3..green_idx.0].trim().parse::<u16>().unwrap();
+        let old_count = game.get(&Color::Green).unwrap();
+        if *old_count < color_count{
+           game.insert(Color::Green, color_count);
+        }
+    }
+
+    for color in Color::iter(){
+        let color_count = game.get(&color).unwrap();
+        let color_limit = limit.get(&color).unwrap();
+        if color_count > color_limit{
+            return 0;
+        }
+    }
+
+    return game_id;
+}
+
+fn power_of_game(line: &str)->u32{
+    let mut game: HashMap<Color, u32> = HashMap::from([(Color::Red, 0), (Color::Green, 0), (Color::Blue, 0)]);
+    let reds: Vec<_> = line.match_indices("red").collect();
+    let greens: Vec<_> = line.match_indices("green").collect();
+    let blues: Vec<_> = line.match_indices("blue").collect();
+
+
+    for red_idx in reds{
+        let color_count = line[red_idx.0-3..red_idx.0].trim().parse::<u32>().unwrap();
+        let old_count = game.get(&Color::Red).unwrap();
+        if *old_count < color_count{
+           game.insert(Color::Red, color_count);
+        }
+    }
+
+    for blue_idx in blues{
+        let color_count = line[blue_idx.0-3..blue_idx.0].trim().parse::<u32>().unwrap();
+        let old_count = game.get(&Color::Blue).unwrap();
+        if *old_count < color_count{
+           game.insert(Color::Blue, color_count);
+        }
+    }
+
+        for green_idx in greens{
+        let color_count = line[green_idx.0-3..green_idx.0].trim().parse::<u32>().unwrap();
+        let old_count = game.get(&Color::Green).unwrap();
+        if *old_count < color_count{
+           game.insert(Color::Green, color_count);
+        }
+    }
+
+    return game.get(&Color::Red).unwrap() * game.get(&Color::Green).unwrap() * game.get(&Color::Blue).unwrap();
+}
+
 
 fn line_num(line: &str)->u32{
     const RADIX: u32 = 10;
@@ -12,7 +101,7 @@ fn line_num(line: &str)->u32{
         }
     }
     let ret = 10* numbers[0] + numbers[numbers.len()-1];
-    println!("{}", ret);
+    // println!("{}", ret);
     ret
 
 }
@@ -76,9 +165,24 @@ fn line_num2(line: &str)->u32{
     // } else{
         ret = 10* digit1 + digit2;
     // }
-    println!("{}", ret);
+    // println!("{}", ret);
     ret
 
+}
+
+fn day2_part1(input_file: &str)-> u16{
+    let limit: HashMap<Color, u16> = HashMap::from([(Color::Red, 12), (Color::Green, 13), (Color::Blue, 14)]);
+    if let Ok(lines) = read_lines(input_file) {
+        return lines.map(|x| game_valid(&x.unwrap(), &limit)).collect::<Vec<u16>>().into_iter().sum()
+    }
+    panic!("file not found")
+}
+
+fn day2_part2(input_file: &str)-> u32{
+    if let Ok(lines) = read_lines(input_file) {
+        return lines.map(|x| power_of_game(&x.unwrap())).collect::<Vec<u32>>().into_iter().sum()
+    }
+    panic!("file not found")
 }
 
 
@@ -98,8 +202,11 @@ fn part2(input_file: &str) -> u32{
 
 
 fn main() {
-    // println!("{}", part1("Inputs/Day1/input1"));
+    println!("{}", part1("Inputs/Day1/input1"));
     println!("{}", part2("Inputs/Day1/input1"));
+    println!("{}", day2_part1("Inputs/Day2/input1"));
+    println!("{}", day2_part2("Inputs/Day2/input1"));
+
 }
 
 #[cfg(test)]
@@ -148,6 +255,11 @@ mod tests {
     #[test]
     fn test_line8() {
         assert_eq!(line_num2("eighjpiigmdt1"), 11);
+    }
+
+    #[test]
+    fn test_example_day2() {
+        assert_eq!(day2_part1("Inputs/Day2/example"), 8);
     }
 
 }
