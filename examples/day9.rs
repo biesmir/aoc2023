7acee203d062edef6b584b85c3f7b0f8142eebbb
Jakub Cebulski <jakubceb@protonmail.com>
2023-12-10 22:13:34 +0100

    day9

--- examples/day9.rs
+++ examples/day9.rs
@@ -54,9 +54,40 @@ fn part1(filename: &str) -> i64 {
     panic!();
 }
 
+fn part2(filename: &str) -> i64 {
+    let mut solution = 0;
+    if let Ok(lines) = read_lines(filename) {
+        for line in lines {
+            let mut numbers_lines: Vec<Vec<i64>> = Vec::new();
+            let line_nums = read_line_to_vec(&line.unwrap());
+            numbers_lines.push(line_nums);
+            loop {
+                let next = make_next_sequence(&numbers_lines[numbers_lines.len() - 1]);
+                numbers_lines.push(next);
+                if numbers_lines[numbers_lines.len() - 1].iter().sum::<i64>() == 0 {
+                    break;
+                }
+            }
+
+            println!("numbers_lines.len() {}", numbers_lines.len());
+            for i in (0..numbers_lines.len()-1).rev(){
+                let tmp = numbers_lines[i][0] - numbers_lines[i+1][0];
+                numbers_lines[i].insert(0, tmp);
+            }
+
+            let tmp = numbers_lines[0][0];
+            println!("tmp {}", tmp);
+            solution += tmp;
+        }
+        return solution;
+    }
+    panic!();
+}
+
+
 fn main() {
     println!("{}", part1("Inputs/Day9/input"));
-    //println!("{}", part2("Inputs/Day9/input"));
+    println!("{}", part2("Inputs/Day9/input"));
 }
 
 #[cfg(test)]
@@ -66,6 +97,10 @@ mod tests {
     #[test]
     fn test_part1_example1() {
         assert_eq!(part1("Inputs/Day9/example"), 114);
+    }
+    #[test]
+    fn test_part2_example1() {
+        assert_eq!(part2("Inputs/Day9/example"), 2);
     }
 }
 
